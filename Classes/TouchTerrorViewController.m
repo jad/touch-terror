fce74dc58d659aded1959ddf210558b7413f7c2f
John A. Debay <john@debay.net>
2010-08-22 13:58:57 -0600

    Added the weapons control.

--- Classes/TouchTerrorViewController.m
+++ Classes/TouchTerrorViewController.m
@@ -10,30 +10,25 @@
 
 @implementation TouchTerrorViewController
 
-
-/*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
-        // Custom initialization
-    }
-    return self;
+- (void)dealloc
+{
+    [super dealloc];
 }
-*/
 
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
-
-
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+
+    NSArray * items = [NSArray arrayWithObjects:@"Rock", @"Fire", @"Flood", @"Lightning", @"Freeze", nil];
+    UISegmentedControl * sc = [[UISegmentedControl alloc] initWithItems:items];
+    [sc setSegmentedControlStyle:UISegmentedControlStyleBar];
+    [sc setSelectedSegmentIndex:0];
+    [sc addTarget:self action:@selector(weaponChanged:) forControlEvents:UIControlEventValueChanged];
+
+    UIBarButtonItem * weaponsItem = [[UIBarButtonItem alloc] initWithCustomView:sc];
+    [[self navigationItem] setLeftBarButtonItem:weaponsItem];
+    [weaponsItem release], weaponsItem = nil;
 }
-*/
 
 
 // Override to allow orientations other than the default portrait orientation.
@@ -48,14 +43,11 @@
 	// Release any cached data, images, etc that aren't in use.
 }
 
-- (void)viewDidUnload {
-	// Release any retained subviews of the main view.
-	// e.g. self.myOutlet = nil;
-}
+#pragma mark -
+#pragma mark Toolbar events
 
-
-- (void)dealloc {
-    [super dealloc];
+- (void)weaponChanged:(UISegmentedControl *)sender
+{
 }
 
 @end
