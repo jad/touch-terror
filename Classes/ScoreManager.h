ddee8df798b42632c8d0b09569aae9d2c3cfa75e
Andy Riordan <andycr512@gmail.com>
2010-08-22 13:50:33 -0600

    Added score manager,

---
+++ Classes/ScoreManager.h
@@ -0,0 +1,20 @@
+//
+//  ScoreManager.h
+//  TouchTerror
+//
+//  Created by Andy Riordan on 8/22/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ScoreManager : NSObject {
+	int score;
+}
+
+@property int score;
+
+- (ScoreManager)defaultManager;
+
+@end
---
+++ Classes/ScoreManager.m
@@ -0,0 +1,26 @@
+//
+//  ScoreManager.m
+//  TouchTerror
+//
+//  Created by Andy Riordan on 8/22/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "ScoreManager.h"
+
+
+@implementation ScoreManager
+
+- (ScoreManager)defaultManager
+{
+	static ScoreManager *shared;
+	
+	if (shared == nil)
+	{
+		shared = [[ScoreManager alloc] init];
+	}
+	
+	return shared;
+}
+
+@end
